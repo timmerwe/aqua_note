c721052d88801a4cb387a003d5eab03c2d563892
tim <timvandemerwe@gmail.com>
2018-09-04 10:30:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+aqua_note
+=========
+
+A Symfony project created on September 4, 2018, 7:10 am.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timvandemerwe
+ * Date: 04-09-18
+ * Time: 09:18
+ */
+
+namespace AppBundle\Controller;
+
+
+class GenusController
+{
+
+}
